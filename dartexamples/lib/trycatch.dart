eb5cae06fb43d86b7a574a4fa804c69ca52ab7d7
Tiago Mesquita <tiagomac@gmail.com>
2020-06-15 23:55:27 -0300

    try catch with rethrow

--- dartexamples/lib/trycatch.dart
+++ dartexamples/lib/trycatch.dart
@@ -17,5 +17,18 @@ class TryCatch {
     } catch (e) {
       print('something is wrong ${e}');
     }
+
+    try {
+      try {
+        throw 'some unexpected error.';
+      } catch (e) {
+        print('exception with throw ${e}');
+        rethrow;
+      }
+    } catch (e) {
+      print('catched the same throw again ${e}');
+    } finally {
+      print('let\'s \'finally\' and get out!');
+    }
   }
 }
